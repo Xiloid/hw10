ea07d227e22d4ea4a09b157b2113fd88c2da17c7
Xiloid <faustov.roman.a@gmail.com>
2021-03-03 18:12:04 +0200

    commited

--- alphabet.py
+++ alphabet.py
@@ -2,23 +2,21 @@ import string
 
 # Алфавит
 class Alphabet:
-
     def __init__(self, language, letters_str):
         self.lang = language
         self.letters = list(letters_str)
-
     # Печатаем все буквы алфавита
+
     def print(self):
         print(self.letters)
-
     # Возвращаем количество букв в алфавите
+
     def letters_num(self):
         len(self.letters)
 
 
 # Английский алфавит
 class EngAlphabet(Alphabet):
-
     __letters_num = 26
 
     def __init__(self):
